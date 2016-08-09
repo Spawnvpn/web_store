9730a5f246d04873347ec05915d66c42f2faaf09
Spawnvpn <spawnvpn@gmail.com>
2016-08-09 15:44:12 +0300

    Update models.py
    Fixed images and increment products on store.

--- goods/models.py
+++ goods/models.py
@@ -21,15 +21,15 @@ class Product(models.Model):
     specifications = models.CharField(max_length=500)
     description = models.TextField(max_length=2000)
     price = models.FloatField()
-    image = ImageField()
-    creator = models.ForeignKey(WebStoreUser)
+    image = models.ImageField()
+    creator = models.ForeignKey(WebStoreUser, null=True)
     quantity_in_stock = models.IntegerField()
 
-    def save(self, *args, **kwargs):
-        if self.image:
-            self.image = get_thumbnail(self.image, '600x600', quality=99,
-                                       format='JPEG').url
-        super(Product, self).save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     if self.image:
+    #         self.image = get_thumbnail(self.image, '600x600', quality=99,
+    #                                    format='JPEG').url
+    #     super(Product, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -49,7 +49,7 @@ class Order(models.Model):
         (DONE, "Done"),
     )
 
-    customer = models.ForeignKey(WebStoreUser)
+    customer = models.ForeignKey(WebStoreUser, null=True)
     total_price = models.FloatField(null=True)
     date = models.DateField(null=True)
     state = models.IntegerField(choices=STATE_CHOICES, default=NEW)
@@ -63,7 +63,7 @@ class Order(models.Model):
 class SubOrder(models.Model):
     order = models.ForeignKey(Order, related_name="suborders")
     product = models.ForeignKey(Product, related_name="suborders_for_product")
-    count = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
 
     def __str__(self):
         return "Sub order for " + str(self.order)
