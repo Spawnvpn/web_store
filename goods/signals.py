d6bb06cfa64cb6356db6f186fc8184edda7ec4b5
Spawnvpn <spawnvpn@gmail.com>
2016-08-09 15:45:11 +0300

    Update signals.py
    Fixed increment products on store.

--- goods/signals.py
+++ goods/signals.py
@@ -1,14 +1,20 @@
-from django.db.models import Count, F
+from django.db import transaction
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from goods.models import Order, Product
+from goods.models import Order, Product, SubOrder
+
+
+@transaction.non_atomic_requests(using='other')
+def decrement_products(qs):
+    for so in qs:
+        product = so.product
+        product.quantity_in_stock -= so.quantity
+        product.save()
 
 
 @receiver(post_save, sender=Order)
 def callback(instance, **kwargs):
     if instance.state == Order.PROCESS:
-        (Product.objects
-                .filter(suborders_for_product__order=instance)
-                .annotate(count=Count("suborders_for_product__count"))
-                .update(quantity_in_stock=F("quantity_in_stock")-F("count")))
+        qs = SubOrder.objects.filter(order=instance).prefetch_related("product")
+        decrement_products(qs)
 
